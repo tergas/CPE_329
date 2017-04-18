8d1f64275491980bfb46908f3039baef9bde7303
Trevor Ergas <trevor.erg@gmail.com>
2017-04-17 17:45:16 -0700

    Add files via upload

---
+++ LCD_code.h
@@ -0,0 +1,112 @@
+/*
+ * LCD_code.h
+ *
+ *  Created on: Apr 13, 2017
+ *      Author: josep_000
+ */
+
+#ifndef LCD_CODE_H_
+#define LCD_CODE_H_
+/* www.MicroDigitalEd.com
+ * p3_3.c: Initialize and display "hello" on the LCD using 4-bit data mode.
+ * Data and control pins share Port 4.
+ * This program does not poll the status of the LCD.
+ * It uses delay to wait out the time LCD controller is busy.
+ * Timing is more relax than the HD44780 datasheet to accommodate the
+ * variations among the LCD modules.
+ * You may want to adjust the amount of delay for your LCD controller.
+ *
+ * Tested with Keil 5.20 and MSP432 Device Family Pack V2.2.0
+ * on XMS432P401R Rev C.
+ */
+#include "msp.h"
+
+#define RS 1     /* P4.0 mask */
+#define RW 2     /* P4.1 mask */
+#define EN 4     /* P4.2 mask */
+
+void delayMs(int n);
+void LCD_nibble_write(unsigned char data, unsigned char control);
+void LCD_command(unsigned char command);
+void LCD_data(unsigned char data);
+void LCD_init(void);
+void Clear_LCD(void);
+void Home_LCD(void);
+void ShiftRight_LCD(void);
+
+void Clear_LCD(void){//clear the LCD
+    LCD_command(1);
+}
+
+void Home_LCD(void){//move cursor home
+    LCD_command(2);
+}
+
+void ShiftRight_LCD(void){//shift cursor to the right
+    LCD_command(0x06);
+}
+
+////////////////////////////////////BOOK CODE BELOW//////////////////////////////////////////////////////////////////////////
+
+void LCD_init(void) {
+    P4->DIR = 0xFF;     /* make P4 pins output for data and controls */
+    delayMs(30);                /* initialization sequence */
+    LCD_nibble_write(0x30, 0);
+    delayMs(10);
+    LCD_nibble_write(0x30, 0);
+    delayMs(1);
+    LCD_nibble_write(0x30, 0);
+    delayMs(1);
+    LCD_nibble_write(0x20, 0);  /* use 4-bit data mode */
+    delayMs(1);
+
+    LCD_command(0x28);      /* set 4-bit data, 2-line, 5x7 font */
+    LCD_command(0x06);      /* move cursor right after each char */
+    LCD_command(0x01);      /* clear screen, move cursor to home */
+    LCD_command(0x0F);      /* turn on display, cursor blinking */
+}
+
+/* With 4-bit mode, each command or data is sent twice with upper
+ * nibble first then lower nibble.
+ */
+void LCD_nibble_write(unsigned char data, unsigned char control) {
+    data &= 0xF0;           /* clear lower nibble for control */
+    control &= 0x0F;        /* clear upper nibble for data */
+    P4->OUT = data | control;       /* RS = 0, R/W = 0 */
+    P4->OUT = data | control | EN;  /* pulse E */
+    delayMs(0);
+    P4->OUT = data;                 /* clear E */
+    P4->OUT = 0;
+}
+
+void LCD_command(unsigned char command) {
+    LCD_nibble_write(command & 0xF0, 0);    /* upper nibble first */
+    LCD_nibble_write(command << 4, 0);      /* then lower nibble */
+
+    if (command < 4)
+        delayMs(4);         /* commands 1 and 2 need up to 1.64ms */
+    else
+        delayMs(1);         /* all others 40 us */
+}
+
+
+void LCD_data(unsigned char data) {
+    LCD_nibble_write(data & 0xF0, RS);    /* upper nibble first */
+    LCD_nibble_write(data << 4, RS);      /* then lower nibble  */
+
+    delayMs(1);
+}
+
+/* delay milliseconds when system clock is at 3 MHz */
+void delayMs(int n) {
+    int i, j;
+
+    for (j = 0; j < n; j++)
+        for (i = 300; i > 0; i--);      /* Delay */
+}
+
+
+
+
+
+#endif /* LCD_CODE_H_ */
