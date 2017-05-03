9a2a5f10e6b2be2e1a7cc633be120ae40f40e788
Trevor Ergas <trevor.erg@gmail.com>
2017-05-03 12:46:40 -0700

    LCD code fasho fasho I'm gettin dat fasho

--- LCD_code.h
+++ LCD_code.h
@@ -21,9 +21,9 @@
  */
 #include "msp.h"
 
-#define RS 1     /* P5.0 mask */
-#define RW 2     /* P5.1 mask */
-#define EN 4     /* P5.2 mask */
+#define RS 1     /* P4.0 mask */
+#define RW 2     /* P4.1 mask */
+#define EN 4     /* P4.2 mask */
 
 void delayMs(int n);
 void LCD_nibble_write(unsigned char data, unsigned char control);
@@ -33,7 +33,6 @@ void LCD_init(void);
 void Clear_LCD(void);
 void Home_LCD(void);
 void ShiftRight_LCD(void);
-void write_to_LCD(char word[]);
 
 void Clear_LCD(void){//clear the LCD
     LCD_command(1);
@@ -47,21 +46,10 @@ void ShiftRight_LCD(void){//shift cursor to the right
     LCD_command(0x06);
 }
 
-void write_to_LCD(char word[16]){
-    int i = 0;
-    LCD_command(1);//clear LCD
-    LCD_command(0x80);//set Cursor home
-    for(i = 0; i < sizeof(word); i++){
-        LCD_data(word[i]);
-    }
-   // Home_LCD();
-}
-
-
 ////////////////////////////////////BOOK CODE BELOW//////////////////////////////////////////////////////////////////////////
 
 void LCD_init(void) {
-    P5->DIR = 0xFF;     /* make P5 pins output for data and controls */
+    P5->DIR = 0xFF;     /* make P4 pins output for data and controls */
     delayMs(30);                /* initialization sequence */
     LCD_nibble_write(0x30, 0);
     delayMs(10);
--- main.c
+++ main.c
@@ -1,177 +1,422 @@
-//*****************************************************************************
-//
-// MSP432 main.c template - Empty main
-//
-//****************************************************************************
+/* p3_5.c: Matrix keypad scanning
+ *
+ * This program scans a 4x4 matrix keypad and returns a unique number
+ * for each key pressed.
+ *
+ * Port 4 7-4 are connected to the columns and Port 4 3-0 are connected
+ * to the rows of the keypad.
+ *
+ * Tested with Keil 5.20 and MSP432 Device Family Pack V2.2.0
+ * on XMS432P401R Rev C.
+ */
 
 #include "msp.h"
-#include "DAC_code.h"
-#include "keyPad_code.h"
-#include <math.h>
+#include "LCD_code.h"
 #include "string.h"
+#include <stdio.h>
+void delay(void);
+void keypad_init(void);
+char keypad_getkey(void);
+void LED_init(void);
+void LED_set(int value);
+void print_key(int value);
+void startUp_screen(void);
+unsigned char check_keys(void);
+int compareArrays(unsigned char attempt[], unsigned char pass[]);
+void helloWorld(void);
+unsigned char * newPass(unsigned char new[]);
+void newPass_screen(void);
+unsigned char poll_keys(void);
 
-#define ONE_VOLT 815//
-#define SQUARE_WAVE 1
-#define SAWTOOTH_WAVE 2
-#define SINE_WAVE 3
-#define INT_ONCE_PER_MS 24000
-#define pi 3.14159265358979323846
-#define TEN_PERCENT 5
-void interrupt_intit(int cctl);
-int waveform = 0;
-int period = 2*INT_ONCE_PER_MS;//2ms or 500Hz
-int frequency = 500;
-int duty_cycle = 50;
-int sine_data[240];
-int main(void)
-{
-    //Initialize sine wave data
-    char word[16];
-    double max_arg = 2*pi;
-        double step = max_arg / (240);
-        double arg = 0;
-        int i = 0;
-        sine_data[0] = sin(0) * ONE_VOLT;
-        for(i = 1; i < 240; i++){
-            arg += step;
-            sine_data[i] = sin(arg) * 2.5 * ONE_VOLT + 2.5 * ONE_VOLT;
-        }
-    //poll keypad for button press
-    //Set up for DAC
-    DAC_init();
-    char key_pressed;
-    keypad_init();
-    LCD_init();
-    TimerA0_Init();
-    while(1){
-        key_pressed = poll_keys();//wait for a button to be pressed
-        delayMs(500);//debounce
-        if(key_pressed > 0 & key_pressed <7){//change frequency
-            switch(key_pressed){
-            case 1 :
-                period = 10  * INT_ONCE_PER_MS;//10ms, 100Hz
-                frequency = 100;
-                strcpy(word, "hello");
-                write_to_LCD("hello");
-                break;
-            case 2 :
-                period = 5 * INT_ONCE_PER_MS;//5ms, 200Hz
-                frequency = 200;
-                break;
-            case 6 :
-                period = 80000;//hard code to 3.33333ms to avoid int math issues
-                frequency = 300;
-                break;         //300Hz
-            case 4 :
-                period = INT_ONCE_PER_MS/2 + 2*INT_ONCE_PER_MS;//2.5ms, 400Hz
-                frequency = 400;
-                break;
-            case 5:
-                period = 2 * INT_ONCE_PER_MS;//2ms, 500Hz
-                frequency = 500;
-                break;
-            default:
-                printf("do nothing");
-            }
-        }
-        else if(key_pressed > 6 & key_pressed < 10){//change the waveform
-            switch(key_pressed){
-            case 7 :
-                waveform = SQUARE_WAVE;
-                break;
-            case 8 :
-                waveform = SAWTOOTH_WAVE;
-                break;
-            case 9 :
-                waveform = SINE_WAVE;
-                break;
-            default :
-                printf("do nothing");
-            }
-        }
-        else{//change the duty cycle when waveform is a square wave
-            switch(key_pressed){
-            case 10 :
-                //if DC > 10%, decrease DC by 10%
-                if(duty_cycle > 10 & waveform == SQUARE_WAVE){
-                    duty_cycle -= TEN_PERCENT;
-                }
-                break;
-            case 11 :
-                //set DC to 50%
-                if(waveform == SQUARE_WAVE){
-                    duty_cycle = 50;
-                }
-                break;
-            case 12 :
-                //if DC < 90%, increase DC by 10%
-                if(duty_cycle < 90 & waveform == SQUARE_WAVE){
-                    duty_cycle += TEN_PERCENT;
-                }
-                break;
-            default :
-                printf("do nothing");
-            }
+int main(void) {
+	//unsigned char key[4];
+	//int correct = 0, j = 0;
+	//unsigned char password[] = {1, 2, 3, 4};
+	keypad_init();
+	LED_init();
+	LCD_init();
+	helloWorld();
 
-            //waveform = SAWTOOTH_WAVE;
-        }
-    }
+	/*while(correct == 0){
+		for(j = 0; j<4; j++){
+			key[j] = '0';
+		}
 
-} // end of main
+		while(1) {
+			key[0] = keypad_getkey();  / read the keypad /
+			if(key[0] > 0){
+				break;
+			}
+		}
+		delayMs(500);
+		if(key[0] == 12){
+			correct = 0;
+			print_key(key[0]);
+			delayMs(500);
+			LCD_init();
+			newPass_screen();
+			for(j = 0; j<4; j++){
+				password[j] = poll_keys();
+				print_key(password[j]);
+				delayMs(500);
+			}
+			LCD_init();
 
+		}
+		else if(key[0] == 10){
+			correct = 0;
+			LCD_init();
+			delayMs(500);
+		}
+		else{
+			print_key(key[0]);
+			P6->DIR &= 0;
+			keypad_init();
 
-//Timer A0 ISR
-void TA0_0_IRQHandler(void) {
-      TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
-      static int isr_square_count = 0;
-      static int isr_sawtooth_count = 0;
-      static int isr_sine_count = 0;
-      switch(waveform){
-          case SQUARE_WAVE :
-                if (isr_square_count > (100-duty_cycle) & isr_square_count < 100){//set to 2V for appropriate amount of time
-                    TempDAC_Value = ONE_VOLT*5;//controls size of step
-                    Drive_DAC(TempDAC_Value);
-                }
-                else if(isr_square_count <= (100-duty_cycle)){//set to 0V for 10ms
-                    TempDAC_Value = 0;
-                    Drive_DAC(TempDAC_Value);
-                }
-                else{//reset the count when reaches 20ms
-                    isr_square_count = 0;
-                }
-              isr_square_count++;
-              TIMER_A0->CCR[0] += (period / 100);//call ISR 100 times per period of the wave
-              break;
-          case SAWTOOTH_WAVE :
-              if (isr_square_count < 200){//set to 2V for 10ms
-                  TempDAC_Value += (ONE_VOLT*5)/200;//controls size of step
-                  Drive_DAC(TempDAC_Value);
-              }
-              else{//reset the count when reaches 20ms
-                  isr_square_count = 0;
-              }
-              isr_square_count++;
-              TIMER_A0->CCR[0] += (period / 200);//call ISR 20 times per period of the wave
-              break;
-          case SINE_WAVE :
-              TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
-              TempDAC_Value = sine_data[isr_sine_count];//controls size of step
-              Drive_DAC(TempDAC_Value);
-              if(isr_sine_count == 239){
-                  isr_sine_count = 0;
-              }
-              isr_sine_count ++;
+			while(1) {
+				key[1] = keypad_getkey();  / read the keypad /
+				if(key[1] > 0){
+					break;
+				}
+			}
+			delayMs(500);
+			if(key[1] == 12){
+				print_key(key[0]);
+				delayMs(500);
+				correct = 0;
+				LCD_init();
+				newPass_screen();
+				for(j = 0; j<4; j++){
+					password[j] = poll_keys();
+					print_key(password[j]);
+					delayMs(500);
+				}
+				LCD_init();
+			}
+			else if(key[1] == 10){
+				correct = 0;
+				LCD_init();
+				delayMs(500);
+			}
+			else{
+				print_key(key[1]);
+				P6->DIR &= 0;
+				keypad_init();
 
-              TIMER_A0->CCR[0] += 24000000/240/frequency;
-              break;
-          default :
-               //set voltae to 0V if no waveform is specified
-               Drive_DAC(0);
+				while(1) {
+					key[2] = keypad_getkey();  / read the keypad /
+					if(key[2] > 0){
+						break;
+					}
+				}
 
-      }
+				delayMs(500);
+				if(key[2] == 12){
+					correct = 0;
+					print_key(key[0]);
+					delayMs(500);
+					LCD_init();
+					newPass_screen();
+					for(j = 0; j<4; j++){
+						password[j] = poll_keys();
+						print_key(password[j]);
+						delayMs(500);
+					}
+					LCD_init();
+				}
+				else if(key[2] == 10){
+					correct = 0;
+					LCD_init();
+					delayMs(500);
+				}
+				else{
+					print_key(key[2]);
+					P6->DIR &= 0;
+					keypad_init();
+
+					while(1) {
+						key[3] = keypad_getkey();  / read the keypad /
+						if(key[3] > 0){
+							break;
+						}
+					}
+					delayMs(500);
+					if(key[3] == 12){
+						correct = 0;
+						print_key(key[0]);
+						delayMs(500);
+						LCD_init();
+						newPass_screen();
+						for(j = 0; j<4; j++){
+							password[j] = poll_keys();
+							print_key(password[j]);
+							delayMs(500);
+						}
+						LCD_init();
+					}
+					else if(key[3] == 10){
+						correct = 0;
+						LCD_init();
+						delayMs(500);
+					}
+					else{
+						print_key(key[3]);
+						P6->DIR &= 0;
+						keypad_init();
+						LCD_init();
+					}
+				}
+			}
+		}
+		if(compareArrays(key, password)){
+
+			correct = 1;
+			helloWorld();
+		}
+		else{
+			startUp_screen();
+		}
 
 
+	}*/
+	return 0;
 }
 
+/* this function initializes Port 4 that is connected to the keypad.
+ * All pins are configured as GPIO input pin. The column pins have
+ * the pull-up resistors enabled.
+ */
+void keypad_init(void) {
+	P6->DIR = 0;
+	P6->REN = 0x70;     /* enable pull resistor for column pins */
+	P6->OUT = 0x70;     /* make column pins pull-ups */
+}
 
+/*
+ * This is a non-blocking function to read the keypad.
+ * If a key is pressed, it returns a unique code for the key. Otherwise,
+ * a zero is returned.
+ * The upper nibble of Port 4 is used as input and connected to the columns.
+ * Pull-up resistors are enabled so when the keys are not pressed, these pins
+ * are pulled high.
+ * The lower nibble of Port 4 is used as output that drives the keypad rows.
+ * First all rows are driven low and the input pins are read. If no key is pressed,
+ * they will read as all one because of the pull up resistors. If they are not
+ * all one, some key is pressed.
+ * If some key is pressed, the program proceeds to drive one row low at a time and
+ * leave the rest of the rows inactive (float) then read the input pins.
+ * Knowing which row is active and which column is active, the program
+ * can decide which key is pressed.
+ *
+ * Only one row is driven so that if multiple keys are pressed and row pins are shorted,
+ * the microcontroller will not be damaged. When the row is being deactivated,
+ * it is driven high first otherwise the stray capacitance may keep the inactive
+ * row low for some time.)
+ */
+char keypad_getkey(void) {
+	int row, col;
+	const char row_select[] = {0x01, 0x02, 0x04, 0x08}; /* one row is active */
 
+	/* check to see any key pressed */
+	P6->DIR |= 0x0F;            /* make all row pins output */
+	P6->OUT &= ~0x0F;           /* drive all row pins low */
+	delay();                    /* wait for signals to settle */
+	col = P6->IN & 0x70;        /* read all column pins */
+	P6->OUT |= 0x0F;            /* drive all rows high before disable them */
+	P6->DIR &= ~0x0F;           /* disable all row pins drive */
+	if (col == 0x70)            /* if all columns are high */
+		return 0;               /* no key pressed */
+
+	/* If a key is pressed, it gets here to find out which key.
+	 * It activates one row at a time and read the input to see
+	 * which column is active. */
+	for (row = 0; row < 4; row++) {
+		P6->DIR &= 0x0F;                /* disable all rows */
+		P6->DIR |= row_select[row];     /* enable one row at a time */
+		P6->OUT &= ~row_select[row];    /* drive the active row low */
+		delay();                        /* wait for signal to settle */
+		col = P6->IN & 0x70;            /* read all columns */
+		P6->OUT |= row_select[row];     /* drive the active row high */
+		if (col != 0x70) break;         /* if one of the input is low, some key is pressed. */
+	}
+	P6->OUT |= 0x0F;                    /* drive all rows high before disable them */
+	P6->DIR &= 0x0F;                    /* disable all rows */
+	if (row == 4)
+		return 0;                       /* if we get here, no key is pressed */
+
+	/* gets here when one of the rows has key pressed, check which column it is */
+	if (col == 0x60) return row * 3 + 1;    /* key in column 0 */
+	if (col == 0x50) return row * 3 + 2;    /* key in column 1 */
+	if (col == 0x30) return row * 3 + 3;    /* key in column 2 */
+	//if (col == 0x70) return row * 4 + 4;    /* key in column 3 */
+
+	return 0;   /* just to be safe */
+}
+
+/* Initialize tri-color LEDs on the LaunchPad board.
+ * P2.0 - red
+ * P2.1 - green
+ * P2.2 - blue
+ */
+void LED_init(void) {
+	P2->DIR = 0x07;         /* make LED pins output */
+	P2->OUT &= ~0x07;       /* turn the LEDs off */
+}
+
+/* turn on or off the LEDs according to bit 2-0 of the value */
+void LED_set(int value) {
+	value &= 0x07;          /* only bit 2-0 are affected */
+	P2->OUT = (P2->OUT & ~0x07) | value;
+}
+
+/* make a small delay */
+void delay(void) {
+}
+
+void print_key(int value){
+	if(value == 1){
+		LCD_data('1');
+	}
+	else if(value == 2){
+		LCD_data('2');
+	}
+	else if(value == 3){
+		LCD_data('3');
+	}
+	else if(value == 4){
+		LCD_data('4');
+	}
+	else if(value == 5){
+		LCD_data('5');
+	}
+	else if(value == 6){
+		LCD_data('6');
+	}
+	else if(value == 7){
+		LCD_data('7');
+	}
+	else if(value == 8){
+		LCD_data('8');
+	}
+	else if(value == 9){
+		LCD_data('9');
+	}
+	else if(value == 10){
+		LCD_data('*');
+	}
+	else if(value == 11){
+		LCD_data('0');
+	}
+	else if(value == 12){
+		LCD_data('#');
+	}
+}
+
+void startUp_screen(void){
+	char lockStart[6] = "LOCKED";
+	char enterKey[10] = "ENTER KEY ";
+	int i = 0;
+
+	for(i = 0; i < sizeof(lockStart); i++){
+		LCD_data(lockStart[i]);
+	}
+
+	LCD_command(0xC0);
+
+	for(i = 0; i < sizeof(enterKey); i++){
+		LCD_data(enterKey[i]);
+	}
+}
+
+void helloWorld(void){
+	char hello[11] = "HELLO WORLD";
+	int i = 0;
+
+	for(i = 0; i < sizeof(hello); i++){
+		LCD_data(hello[i]);
+	}
+}
+
+int compareArrays(unsigned char attempt[], unsigned char pass[]){
+	int j = 0;
+	for(j = 0; j < 4; j++){
+		if(attempt[j] != pass
+				[j]){
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+unsigned char poll_keys(void){
+	char key = 0;
+	while(key <= 0){
+		key = keypad_getkey();
+	}
+	return key;
+}
+
+/*unsigned char * newPass(unsigned char* new){
+  while(1){
+ *new = keypad_getkey();  /* read the keypad /
+ if(*new > 0){
+ break;
+ }
+ }
+
+ delayMs(500);
+ print_key(*new);
+
+ while(1){
+ *(new+1) = keypad_getkey();
+ if(*(new+1) > 0){
+ break;
+ }
+ }
+
+ delayMs(500);
+ print_key(*(new+1));
+
+ while(1){
+ *(new+2) = keypad_getkey();
+ if(*(new+2) > 0){
+ break;
+ }
+ }
+
+ delayMs(500);
+ print_key(*(new+2));
+
+ while(1){
+ *(new+3) = keypad_getkey();
+ if(*(new+3) > 0){
+ break;
+ }
+ }
+
+ delayMs(500);
+ print_key(*(new+3));
+
+ *(new+4) = '\0';
+
+ return new;
+
+ }*/
+
+void newPass_screen(void){
+	char line_1[12] = "NEW PASSWORD";
+	char line_2[9] = "ENTER KEY";
+	int i;
+
+	for(i = 0; i < sizeof(line_1); i++){
+		LCD_data(line_1[i]);
+	}
+
+	LCD_command(0xC0);
+
+	for(i = 0; i < sizeof(line_2); i++){
+		LCD_data(line_2[i]);
+	}
+
+}
