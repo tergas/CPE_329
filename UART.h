fcca21e9f5c60062925179ccac5815ce76367700
josephschnecker <josephschnecker95@yahoo.com>
2017-05-11 13:44:30 -0700

    Add files via upload

---
+++ UART.h
@@ -0,0 +1,35 @@
+/*
+ * UART.h
+ *
+ *  Created on: May 11, 2017
+ *      Author: josep_000
+ */
+
+#ifndef UART_H_
+#define UART_H_
+#include "msp.h"
+
+void UART0_init(void);
+void Write_to_terminal(char character);
+void UART_INIT_init(void);
+
+void UART0_init(void) {
+    EUSCI_A0->CTLW0 |= 1;     /* put in reset mode for config */
+    EUSCI_A0->MCTLW = 0;      /* disable oversampling */
+    EUSCI_A0->CTLW0 = 0x0081; /* 1 stop bit, no parity, SMCLK, 8-bit data */
+    EUSCI_A0->BRW = 26;       /* 3000000 / 115200 = 26 */
+    P1->SEL0 |= 0x0C;         /* P1.3, P1.2 for UART */
+    P1->SEL1 &= ~0x0C;
+    EUSCI_A0->CTLW0 &= ~1;    /* take UART out of reset mode */
+}
+
+void Write_to_terminal(char character){
+    while(!(EUSCI_A0->IFG & 0x02)) { }  /* wait until receive buffer is full */
+    EUSCI_A0-> TXBUF = character;
+}
+
+void UART_INT_init(void){
+    EUSCI_A0->IE |= 1;        /* enable receive interrupt */
+}
+
+#endif /* UART_H_ */
--- main.c
+++ main.c
@@ -1,421 +1,157 @@
-/* p3_5.c: Matrix keypad scanning
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2013, Texas Instruments Incorporated
+ * All rights reserved.
  *
- * This program scans a 4x4 matrix keypad and returns a unique number
- * for each key pressed.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * Port 4 7-4 are connected to the columns and Port 4 3-0 are connected
- * to the rows of the keypad.
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * Tested with Keil 5.20 and MSP432 Device Family Pack V2.2.0
- * on XMS432P401R Rev C.
- */
-
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP432 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP432 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see http://www.ti.com/tool/mspdriverlib for an API functional
+ * library & https://dev.ti.com/pinmux/ for a GUI approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//******************************************************************************
+//  MSP432P401 Demo - ADC14, Sample A1, AVcc Ref, Set P1.0 if A1 > 0.5*AVcc
+//
+//   Description: A single sample is made on A1 with reference to AVcc.
+//   Software sets ADC14_CTL0_SC to start sample and conversion - ADC14_CTL0_SC
+//   automatically cleared at EOC. ADC14 internal oscillator times sample (16x)
+//   and conversion. In Mainloop MSP432 waits in LPM0 to save power until ADC14
+//   conversion complete, ADC14_ISR will force exit from LPM0 in Mainloop on
+//   reti. If A0 > 0.5*AVcc, P1.0 set, else reset. The full, correct handling of
+//   and ADC14 interrupt is shown as well.
+//
+//
+//                MSP432P401x
+//             -----------------
+//         /|\|              XIN|-
+//          | |                 |
+//          --|RST          XOUT|-
+//            |                 |
+//        >---|P5.4/A1      P1.0|-->LED
+//
+//   William Goh
+//   Texas Instruments Inc.
+//   June 2016 (updated) | November 2013 (created)
+//   Built with CCSv6.1, IAR, Keil, GCC
+//******************************************************************************
 #include "msp.h"
-#include "LCD_code.h"
-#include "string.h"
-#include <stdio.h>
-void delay(void);
-void keypad_init(void);
-char keypad_getkey(void);
-void LED_init(void);
-void LED_set(int value);
-void print_key(int value);
-void startUp_screen(void);
-unsigned char check_keys(void);
-int compareArrays(unsigned char attempt[], unsigned char pass[]);
-void helloWorld(void);
-unsigned char * newPass(unsigned char new[]);
-void newPass_screen(void);
-unsigned char poll_keys(void);
+#include "UART.h"
+#define TEN_MILI_VOLTS 49
+static int ADC_UPDATED = 1;//static variable to limit scope to this file
+static int ADC_VALUE = 0;
+static int calibrated_adc_val;
+
+int Check_ADC(void);//checks if ADC is updated, ADC_UPDATED can be accessed from other files
+int Get_ADC_VALUE();
+int calibrate_ADC_VALUE(void);
 
 int main(void) {
-    unsigned char key[4];
-    int correct = 0, j = 0;
-    unsigned char password[] = {1, 2, 3, 4};
-    keypad_init();
-    LED_init();
-    LCD_init();
-    startUp_screen();
+    volatile unsigned int i;
 
-while(correct == 0){
-    for(j = 0; j<4; j++){
-        key[j] = '0';
-    }
+    WDT_A->CTL = WDT_A_CTL_PW |             // Stop WDT
+                 WDT_A_CTL_HOLD;
 
-    while(1) {
-        key[0] = keypad_getkey();  /* read the keypad */
-            if(key[0] > 0){
-                break;
-           }
-   }
-    delayMs(500);
-    if(key[0] == 12){
-        correct = 0;
-        print_key(key[0]);
-        delayMs(500);
-        LCD_init();
-        newPass_screen();
-        for(j = 0; j<4; j++){
-            password[j] = poll_keys();
-            print_key(password[j]);
-            delayMs(500);
-        }
-        LCD_init();
+    // GPIO Setup
+    P1->OUT &= ~BIT0;                       // Clear LED to start
+    P1->DIR |= BIT0;                        // Set P1.0/LED to output
+    P5->SEL1 |= BIT4;                       // Configure P5.4 for ADC
+    P5->SEL0 |= BIT4;
 
-    }
-    else if(key[0] == 10){
-        correct = 0;
-        LCD_init();
-        delayMs(500);
-    }
-    else{
-        print_key(key[0]);
-        P6->DIR &= 0;
-        keypad_init();
+    // Enable global interrupt
+    __enable_irq();
 
-        while(1) {
-            key[1] = keypad_getkey();  /* read the keypad */
-                if(key[1] > 0){
-                    break;
-                }
-        }
-        delayMs(500);
-        if(key[1] == 12){
-            print_key(key[0]);
-            delayMs(500);
-            correct = 0;
-            LCD_init();
-            newPass_screen();
-            for(j = 0; j<4; j++){
-                password[j] = poll_keys();
-                print_key(password[j]);
-                delayMs(500);
-            }
-            LCD_init();
-        }
-        else if(key[1] == 10){
-            correct = 0;
-            LCD_init();
-            delayMs(500);
-        }
-        else{
-            print_key(key[1]);
-            P6->DIR &= 0;
-            keypad_init();
+    // Enable ADC interrupt in NVIC module
+    NVIC->ISER[0] = 1 << ((ADC14_IRQn) & 31);
 
-            while(1) {
-                key[2] = keypad_getkey();  /* read the keypad */
-                    if(key[2] > 0){
-                        break;
-                    }
-            }
+    // Sampling time, S&H=16, ADC14 on
+    ADC14->CTL0 = ADC14_CTL0_SHT0_2 | ADC14_CTL0_SHP | ADC14_CTL0_ON;
+    ADC14->CTL1 = ADC14_CTL1_RES_3;         // Use sampling timer, 14-bit conversion results
 
-            delayMs(500);
-            if(key[2] == 12){
-                correct = 0;
-                print_key(key[0]);
-                delayMs(500);
-                LCD_init();
-                newPass_screen();
-                for(j = 0; j<4; j++){
-                    password[j] = poll_keys();
-                    print_key(password[j]);
-                    delayMs(500);
-                }
-                LCD_init();
-            }
-            else if(key[2] == 10){
-                correct = 0;
-                LCD_init();
-                delayMs(500);
-            }
-            else{
-                print_key(key[2]);
-                P6->DIR &= 0;
-                keypad_init();
+    ADC14->MCTL[0] |= ADC14_MCTLN_INCH_1;   // A1 ADC input select; Vref=AVCC
+    ADC14->IER0 |= ADC14_IER0_IE0;          // Enable ADC conv complete interrupt
 
-                while(1) {
-                    key[3] = keypad_getkey();  /* read the keypad */
-                        if(key[3] > 0){
-                            break;
-                        }
-                }
-                delayMs(500);
-                if(key[3] == 12){
-                    correct = 0;
-                    print_key(key[0]);
-                    delayMs(500);
-                    LCD_init();
-                    newPass_screen();
-                    for(j = 0; j<4; j++){
-                        password[j] = poll_keys();
-                        print_key(password[j]);
-                        delayMs(500);
-                    }
-                    LCD_init();
-                }
-                else if(key[3] == 10){
-                    correct = 0;
-                    LCD_init();
-                    delayMs(500);
-                }
-                else{
-                    print_key(key[3]);
-                    P6->DIR &= 0;
-                    keypad_init();
-                    LCD_init();
-                }
-            }
-        }
-    }
-        if(compareArrays(key, password)){
+    SCB->SCR &= ~SCB_SCR_SLEEPONEXIT_Msk;   // Wake up on exit from ISR
 
-            correct = 1;
-            helloWorld();
-        }
-        else{
-            startUp_screen();
+    UART0_init();//set up UART to write to terminal
+    Write_to_terminal('B');
+
+    while (1)
+    {
+        for (i = 20000; i > 0; i--);        // Delay
+
+        // Start sampling/conversion
+        if(ADC_UPDATED){
+            calibrated_adc_val = calibrate_ADC_VALUE();
+            ADC14->CTL0 |= ADC14_CTL0_ENC | ADC14_CTL0_SC;
+            ADC_UPDATED = 0;
         }
 
-
     }
 }
 
-/* this function initializes Port 4 that is connected to the keypad.
- * All pins are configured as GPIO input pin. The column pins have
- * the pull-up resistors enabled.
- */
-void keypad_init(void) {
-    P6->DIR = 0;
-    P6->REN = 0x70;     /* enable pull resistor for column pins */
-    P6->OUT = 0x70;     /* make column pins pull-ups */
+int Check_ADC(void){
+    return ADC_UPDATED;
 }
 
-/*
- * This is a non-blocking function to read the keypad.
- * If a key is pressed, it returns a unique code for the key. Otherwise,
- * a zero is returned.
- * The upper nibble of Port 4 is used as input and connected to the columns.
- * Pull-up resistors are enabled so when the keys are not pressed, these pins
- * are pulled high.
- * The lower nibble of Port 4 is used as output that drives the keypad rows.
- * First all rows are driven low and the input pins are read. If no key is pressed,
- * they will read as all one because of the pull up resistors. If they are not
- * all one, some key is pressed.
- * If some key is pressed, the program proceeds to drive one row low at a time and
- * leave the rest of the rows inactive (float) then read the input pins.
- * Knowing which row is active and which column is active, the program
- * can decide which key is pressed.
- *
- * Only one row is driven so that if multiple keys are pressed and row pins are shorted,
- * the microcontroller will not be damaged. When the row is being deactivated,
- * it is driven high first otherwise the stray capacitance may keep the inactive
- * row low for some time.)
- */
-char keypad_getkey(void) {
-    int row, col;
-    const char row_select[] = {0x01, 0x02, 0x04, 0x08}; /* one row is active */
-
-    /* check to see any key pressed */
-    P6->DIR |= 0x0F;            /* make all row pins output */
-    P6->OUT &= ~0x0F;           /* drive all row pins low */
-    delay();                    /* wait for signals to settle */
-    col = P6->IN & 0x70;        /* read all column pins */
-    P6->OUT |= 0x0F;            /* drive all rows high before disable them */
-    P6->DIR &= ~0x0F;           /* disable all row pins drive */
-    if (col == 0x70)            /* if all columns are high */
-        return 0;               /* no key pressed */
-
-    /* If a key is pressed, it gets here to find out which key.
-     * It activates one row at a time and read the input to see
-     * which column is active. */
-    for (row = 0; row < 4; row++) {
-        P6->DIR &= 0x0F;                /* disable all rows */
-        P6->DIR |= row_select[row];     /* enable one row at a time */
-        P6->OUT &= ~row_select[row];    /* drive the active row low */
-        delay();                        /* wait for signal to settle */
-        col = P6->IN & 0x70;            /* read all columns */
-        P6->OUT |= row_select[row];     /* drive the active row high */
-        if (col != 0x70) break;         /* if one of the input is low, some key is pressed. */
-    }
-    P6->OUT |= 0x0F;                    /* drive all rows high before disable them */
-    P6->DIR &= 0x0F;                    /* disable all rows */
-    if (row == 4)
-        return 0;                       /* if we get here, no key is pressed */
-
-    /* gets here when one of the rows has key pressed, check which column it is */
-    if (col == 0x60) return row * 3 + 1;    /* key in column 0 */
-    if (col == 0x50) return row * 3 + 2;    /* key in column 1 */
-    if (col == 0x30) return row * 3 + 3;    /* key in column 2 */
-    //if (col == 0x70) return row * 4 + 4;    /* key in column 3 */
-
-    return 0;   /* just to be safe */
+int calibrate_ADC_VALUE(void){
+    float temp = (ADC_VALUE / TEN_MILI_VOLTS)*10;
+    (int) temp;
+    return temp;
 }
 
-/* Initialize tri-color LEDs on the LaunchPad board.
- * P2.0 - red
- * P2.1 - green
- * P2.2 - blue
- */
-void LED_init(void) {
-    P2->DIR = 0x07;         /* make LED pins output */
-    P2->OUT &= ~0x07;       /* turn the LEDs off */
-}
-
-/* turn on or off the LEDs according to bit 2-0 of the value */
-void LED_set(int value) {
-    value &= 0x07;          /* only bit 2-0 are affected */
-    P2->OUT = (P2->OUT & ~0x07) | value;
-}
-
-/* make a small delay */
-void delay(void) {
-}
-
-void print_key(int value){
-    if(value == 1){
-        LCD_data('1');
-    }
-    else if(value == 2){
-        LCD_data('2');
-    }
-    else if(value == 3){
-        LCD_data('3');
-    }
-    else if(value == 4){
-        LCD_data('4');
-    }
-    else if(value == 5){
-        LCD_data('5');
-    }
-    else if(value == 6){
-        LCD_data('6');
-    }
-    else if(value == 7){
-        LCD_data('7');
-    }
-    else if(value == 8){
-        LCD_data('8');
-    }
-    else if(value == 9){
-        LCD_data('9');
-    }
-    else if(value == 10){
-        LCD_data('*');
-    }
-    else if(value == 11){
-        LCD_data('0');
-    }
-    else if(value == 12){
-        LCD_data('#');
-    }
-}
-
-void startUp_screen(void){
-    char lockStart[6] = "LOCKED";
-    char enterKey[10] = "ENTER KEY ";
-    int i = 0;
-
-    for(i = 0; i < sizeof(lockStart); i++){
-            LCD_data(lockStart[i]);
-        }
-
-    LCD_command(0xC0);
-
-    for(i = 0; i < sizeof(enterKey); i++){
-        LCD_data(enterKey[i]);
-    }
-}
-
-void helloWorld(void){
-    char hello[11] = "HELLO WORLD";
-    int i = 0;
-
-    for(i = 0; i < sizeof(hello); i++){
-        LCD_data(hello[i]);
-    }
-}
-
-int compareArrays(unsigned char attempt[], unsigned char pass[]){
-    int j = 0;
-    for(j = 0; j < 4; j++){
-            if(attempt[j] != pass
-                    [j]){
-                return 0;
-            }
-    }
-
-    return 1;
-}
-
-unsigned char poll_keys(void){
-    char key = 0;
-    while(key <= 0){
-        key = keypad_getkey();
-    }
-    return key;
-}
-
-/*unsigned char * newPass(unsigned char* new){
-    while(1){
-        *new = keypad_getkey();  /* read the keypad /
-            if(*new > 0){
-                break;
-            }
-    }
-
-    delayMs(500);
-    print_key(*new);
-
-    while(1){
-        *(new+1) = keypad_getkey();
-            if(*(new+1) > 0){
-                break;
-            }
-    }
-
-    delayMs(500);
-    print_key(*(new+1));
-
-    while(1){
-        *(new+2) = keypad_getkey();
-            if(*(new+2) > 0){
-                break;
-            }
-    }
-
-    delayMs(500);
-    print_key(*(new+2));
-
-    while(1){
-        *(new+3) = keypad_getkey();
-            if(*(new+3) > 0){
-                break;
-            }
-    }
-
-    delayMs(500);
-    print_key(*(new+3));
-
-    *(new+4) = '\0';
-
-    return new;
-
-}*/
-
-void newPass_screen(void){
-    char line_1[12] = "NEW PASSWORD";
-    char line_2[9] = "ENTER KEY";
+void print_value_to_terminal(int num){
     int i;
-
-    for(i = 0; i < sizeof(line_1); i++){
-            LCD_data(line_1[i]);
+    int divider = 1000;
+    for(i=0; i< 4; i++){
+        if(i == 1){
+            Write_to_terminal('.');
         }
-
-    LCD_command(0xC0);
-
-    for(i = 0; i < sizeof(line_2); i++){
-        LCD_data(line_2[i]);
+        else{
+            int temp = num/divider;
+            Write_to_terminal((char) temp);
+            divider /= 10;
+            num %= 10;
+        }
     }
+}
 
+// ADC14 interrupt service routine
+void ADC14_IRQHandler(void) {
+   // int ADC_val;
+    ADC_VALUE = ADC14->MEM[0];//1240 = 1 Volt
+    ADC_UPDATED = 1;
 }
